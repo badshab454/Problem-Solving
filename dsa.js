48904566e9d0de1146ff227d4905c560b9c6b9f4
Badsha Hossain <badshab454@gmail.com>
2024-02-27 22:39:50 +0600

    reverse case

--- dsa.js
+++ dsa.js
@@ -1,21 +1,13 @@
-// Given an array of N distinct elements, the task is to find all elements in array except two greatest elements in sorted order.
-// Example 1:
-// Input : 
-// a[] = {2, 8, 7, 1, 5}
-// Output :
-// 1 2 5 
-// Explanation :
-// The output three elements have two or
-// more greater elements.   
+// reverseCase("Happy Birthday") ➞ "hAPPY bIRTHDAY"
 
+function reverseCase(x) {
+    // Example 1:
 
-function findElements(arr,n){
-    //code here
-    let sortedArr = arr.sort((a, b) => a - b);
-    sortedArr.pop();
-    sortedArr.pop();
-    return sortedArr;
- }
+    // Input: x = 121
+    // Output: true
+    let num = x.toString().split("").reverse().join("");
+    let revNum = parseInt(num);
 
-console.log(findElements([10, 11, 4]));
-
+    return revNum == x ? true : false;
+}
+console.log(reverseCase(123));
