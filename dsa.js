5e6a11d7d7364805be9d504cb8bc05552dd4c3ab
Badsha Hossain <badshab454@gmail.com>
2024-03-04 11:24:25 +0600

    searching number

--- dsa.js
+++ dsa.js
@@ -1,17 +1,14 @@
-//is same for last number
+// Input:
+// n = 5
+// k = 16
+// arr = {9, 7, 2, 16, 4}
+// Output: 4
 
-function isSame(s) {
-  //code here
-  let str = "";
-  let numStr = "";
-  for (let i = 0; i < s.length; i++) {
-    if (!isNaN(s[i])) {
-      numStr += s[i];
-    } else {
-      str += s[i];
-    }
-  }
-  return parseInt(numStr) == str.length ? 1 : 0;
+
+function search(n, k, arr) {
+  // code here
+  let plusK = arr.indexOf(k) + 1;
+  return plusK === 0 ? -1 : plusK;
 }
 
-console.log(isSame("gypgqgraaaa011"));
+console.log(search(5, 16, [9, 7, 2, 16, 4]));
