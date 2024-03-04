11fab95304305ad294ca9a1211180b1080cd7fbb
Badsha Hossain <badshab454@gmail.com>
2024-03-04 12:13:55 +0600

    remove character

--- dsa.js
+++ dsa.js
@@ -1,14 +1,28 @@
-// Input:
-// n = 5
-// k = 16
-// arr = {9, 7, 2, 16, 4}
-// Output: 4
+// Remove character
+// string1 = "computer"
+// string2 = "cat"
+// Output: "ompuer"
+// Explanation: After removing characters(c, a, t)
+// from string1 we get "ompuer".
 
+// let result = '';
+//     for (let i = 0; i < str1.length; i++) {
+//         if (!str2.includes(str1[i])) {
+//             result += str1[i];
+//         }
+//     }
+//     return result;
 
-function search(n, k, arr) {
-  // code here
-  let plusK = arr.indexOf(k) + 1;
-  return plusK === 0 ? -1 : plusK;
+function removeChars(s1, s2){
+  //code here
+  let result = '';
+  for (let i = 0; i < s1.length; i++){
+    if (!s2.includes(s1[i])){
+      result += s1[i];
+    }
+  }
+  return result;
 }
 
-console.log(search(5, 16, [9, 7, 2, 16, 4]));
+console.log(removeChars("computer", "cat"));
+
