d09b9628f4d26676a469ce783a4c0a3aef1e9063
Badsha Hossain <badshab454@gmail.com>
2024-04-27 15:21:26 +0600

    find duplicate in array

--- dsa.js
+++ dsa.js
@@ -1,12 +1,41 @@
-//Reverse words in a given string
+// Find duplicates in an array
 
-//vvvvvvvInput:
-// S = i.like.this.program.very.much
-// Output: much.very.program.this.like.i
+// Example 1:
 
-function reverseWords(s) {
-  // code here
-  return s.split('.').reverse().join(".");
+// Input:
+// N = 4
+// a[] = {0,3,1,2}
+// Output:
+// -1
+// Explanation:
+// There is no repeating element in the array. Therefore output is -1.
+// Example 2:
+
+// Input:
+// N = 5
+// a[] = {2,3,1,2,3}
+// Output:
+// 2 3
+// Explanation:
+// 2 and 3 occur more than once in the given array.
+
+function duplicates(a, n) {
+  //your code here
+  let result = [];
+    let count = new Array(n).fill(0);
+
+    for (let i = 0; i < n; i++) {
+        count[a[i]]++;
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (count[i] > 1) {
+            result.push(i);
+        }
+    }
+
+    return result.length > 0 ? result : [-1];
+  
 }
 
-console.log(reverseWords("i.like.this.program.very.much"));
+console.log(duplicates([1, 1, 1, 2, 2, 4], 6));
