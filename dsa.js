8fe035fdfb9b92f67910450f0bc2342bb5fb115e
Badsha Hossain <badshab454@gmail.com>
2024-05-16 12:48:59 +0600

    1281 leetcode

--- dsa.js
+++ dsa.js
@@ -1,21 +1,24 @@
-// 9. Palindrome Number
+// 1281. Subtract the Product and Sum of Digits of an Integer
 
-// Given an integer x, return true if x is a 
-// palindrome
-// , and false otherwise.
-
+// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
  
 
-// Example 1:`
+// Example 1:
 
-// Input: x = 121
-// Output: true
-// Explanation: 121 reads as 121 from left to right and from right to left.
+// Input: n = 234
+// Output: 15 
+// Explanation: 
+// Product of digits = 2 * 3 * 4 = 24 
+// Sum of digits = 2 + 3 + 4 = 9 
+// Result = 24 - 9 = 15
 
-function isPalindrome (x) {
-    let arr = x.toString().split('').reverse().join('')
-    let revNum = parseInt(arr);
-    return x == revNum;
-}
+var subtractProductAndSum = function(n) {
+    n = n.toString().split('');
 
-console.log(isPalindrome(123));
+    let product = n.reduce((accu, curr) => accu * curr);
+    let sum = n.reduce((accu, curr) => accu + Number(curr), 0);
+    
+    return product - sum;
+};
+
+console.log(subtractProductAndSum(234));
