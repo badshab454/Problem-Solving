22325f6d917407ee3e9ca44e76940356b85c4df1
Badsha Hossain <badshab454@gmail.com>
2024-05-20 16:34:42 +0600

    reverse words in a string

--- dsa.js
+++ dsa.js
@@ -1,24 +1,20 @@
-// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-
-// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-
- 
-
+// 557. Reverse Words in a String III
+// Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
 // Example 1:
-
-// Input: s = "anagram", t = "nagaram"
-// Output: true
+// Input: s = "Let's take LeetCode contest"
+// Output: "s'teL ekat edoCteeL tsetnoc"
 // Example 2:
+// nput: s = "Mr Ding"
+// Output: "rM gniD"
+// Constraints:
+// 1 <= s.length <= 5 * 104
+// s contains printable ASCII characters.
+// s does not contain any leading or trailing spaces.
+// There is at least one word in s.
+// All the words in s are separated by a single space.
 
-// Input: s = "rat", t = "car"
-// Output: false
-var isAnagram = function(s, t) {
-    s = s.split('').sort((a, b) => a.localeCompare(b)).join('');
-    t = t.split('').sort((a, b) => a.localeCompare(b)).join('');
-    return s == t;
+var reverseWords = function(s) {
+    return s.split(' ').map((val) => val.split('').reverse().join('')).join(' ')
 };
 
-console.log(isAnagram('anagram', 'nagaam'));
-
-
-
+console.log(reverseWords('Mr Ding'));
