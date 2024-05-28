932be48f6fd7bcdba9480f0b4fcf2f03dff2f390
Badsha Hossain <badshab454@gmail.com>
2024-05-28 12:30:28 +0600

    goal parser interpretation

--- dsa.js
+++ dsa.js
@@ -1,31 +1,22 @@
-function rearrange (arr, n) {
-    let pos = [];
-    let neg = [];
-    let result = [];
-    for (let i = 0; i < n; i++) {
-        if (arr[i] >= 0) {
-            pos.push(arr[i]);
-        } else {
-            neg.push(arr[i]);
-        }
-    }
-    let i = 0; 
-    let j = 0; 
-    while (i < pos.length && j < neg.length) {
-        result.push(pos[i]);
-        result.push(neg[j]);
-        i++;
-        j++;
-    }
-    while (i < pos.length) {
-        result.push(pos[i]);
-        i++
-    }
-    while (j < neg.length) {
-        result.push(neg[j]);
-        j++
-    }
-    return result;
-}
+// 1678. Goal Parser Interpretation
+// You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+// Given the string command, return the Goal Parser's interpretation of command.
+// Example 1:
+// Input: command = "G()(al)"
+// Output: "Goal"
+// Explanation: The Goal Parser interprets the command as follows:
+// G -> G
+// () -> o
+// (al) -> al
+// The final concatenated result is "Goal".
 
-console.log(rearrange([9, 4, -2, -1, 5, 0, -5, -3, 2], 9));
+var interpret = function(command) {
+    return command.replaceAll("()", 'o').replaceAll("(al)", "al");
+};
+
+console.log(interpret("G()(al)"));
+
+
+
+
+
