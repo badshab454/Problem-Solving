c50189d8e7fbfe545af7bb195035a588e55616df
Badsha Hossain <badshab454@gmail.com>
2024-06-08 13:54:38 +0600

    189. Rotate Array

--- dsa.js
+++ dsa.js
@@ -1,22 +1,24 @@
-// 88. Merge Sorted Array
-// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
-// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
-// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+// 189. Rotate Array
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 // Example 1:
-// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-// Output: [1,2,2,3,5,6]
-// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
-// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
-function merge (nums1, m, nums2, n) {
-  let i = 0; 
-  for (let j = nums1.length - 1; j >= m; j--) {
-    if (nums1[j] == 0) {
-      nums1.splice(j, 1);
-      nums1.push(nums2[i]);
-      i++
+var rotate = function(nums, k) {
+    let n = nums.length;
+    k = k % n;
+    let result = new Array(n);
+    for (let i = 0; i < n; i++) {
+      result[(i + k) % n] = nums[i];
     }
+    for (let i = 0; i < n; i++) {
+      nums[i] = result[i];
+    }
+    return nums;
   }
-  return nums1.sort();
-}
-console.log(merge([1,2,3,0,0,0], 3, [2, 5, 6], 3));
+
+console.log(rotate([1,2,3,4,5,6,7], 3));
