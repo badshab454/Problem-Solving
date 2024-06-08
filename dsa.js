c0713ade68ca782d993f9f90b26dfa4b86caa1d3
Badsha Hossain <badshab454@gmail.com>
2024-06-08 13:02:50 +0600

    88. merge sorted array

--- dsa.js
+++ dsa.js
@@ -1,18 +1,22 @@
-// 2535. Difference Between Element Sum and Digit Sum of an Array
+// 88. Merge Sorted Array
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 // Example 1:
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
 
-// Input: nums = [1,15,6,3]
-// Output: 9
-// Explanation: 
-// The element sum of nums is 1 + 15 + 6 + 3 = 25.
-// The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
-// The absolute difference between the element sum and digit sum is |25 - 16| = 9.
-``
-var differenceOfSum = function(nums) {
-  let elementSum = nums.reduce((a, b) => Number(a) + Number(b));
-  let digitArr = nums.join('').split('');
-  let digitSum = digitArr.reduce((c, d) => Number(c) + Number(d));
-  return elementSum >= digitSum ? elementSum - digitSum : digitSum - elementSum; 
-};
-
-console.log(differenceOfSum([1,15,6,3]));
+function merge (nums1, m, nums2, n) {
+  let i = 0; 
+  for (let j = nums1.length - 1; j >= m; j--) {
+    if (nums1[j] == 0) {
+      nums1.splice(j, 1);
+      nums1.push(nums2[i]);
+      i++
+    }
+  }
+  return nums1.sort();
+}
+console.log(merge([1,2,3,0,0,0], 3, [2, 5, 6], 3));
