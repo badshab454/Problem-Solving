b8134e91124c31987cb066a3239e47a78cf3d1af
Badsha Hossain <badshab454@gmail.com>
2024-07-01 15:15:16 +0600

    merge sorted array

--- dsa.js
+++ dsa.js
@@ -1,23 +1,29 @@
-// 45. Jump Game II
+// 88. Merge Sorted Array
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+ 
 // Example 1:
 
-// Input: nums = [2,3,1,1,4]
-// Output: 2
-// Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+// Example 2:
 
 
-var jump = function(nums) {
-    let jumps = 0;
-    let currEnd = 0;
-    let farthest = 0;
-    for (let i = 0; i < nums.length - 1; i++) {
-        farthest = Math.max(farthest, i + nums[i]);
-        if (i === currEnd) {
-            jumps++;
-            currEnd = farthest;
+var merge = function(nums1, m, nums2, n) {
+    let j = 0;
+    for (let i = nums1.length; i >= m; i--) {
+        if (nums1[i] == '0') {
+            nums1[i] = nums2[j];
+            j++;
         }
     }
-    return jumps;
+    return nums1.sort((a, b) => a - b);
 };
 
-console.log(jump([2,3,1,1,4]))
+console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3));
